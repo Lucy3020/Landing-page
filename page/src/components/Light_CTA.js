ab66cb763b02cfb388f0ffea9a79db1b390e60aa
Lucy3020 <67004463+Lucy3020@users.noreply.github.com>
2022-09-20 17:04:33 +0700

    add light_CTA

---
+++ page/src/components/Light_CTA.js
@@ -0,0 +1,26 @@
+import './light_CTA.css';
+import '../font/Graphik-Regular.ttf';
+import screen2 from '../imgic/screen_content2.png';
+function Light_CTA()
+{
+     return (
+  <div className='light_CTA'>
+   <img className='screen2'src = {screen2} alt=''/>
+      <div className='block'>
+        <h3>At your fingertips</h3>
+        <h2>Lightning fast prototyping</h2>
+        <div className='block2'>
+          <h3>Subscribe to our Newsletter</h3>
+          <p>Available exclusivery on Figmaland</p>
+        </div>
+        <span>
+          <input className="input" type="text" value={"Your email"}></input>
+          <button className='button3'>Subscribe</button>
+        </span>
+        
+      </div>
+  </div>
+   );  
+}
+
+export default Light_CTA;
