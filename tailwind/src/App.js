37251e6b371de7dad9a3c2df595247c01ddb0a9d
Lucy3020 <67004463+Lucy3020@users.noreply.github.com>
2022-10-05 16:16:39 +0700

    add pricing-page with tailwind

---
+++ tailwind/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import './font/Graphik-Regular.ttf';
+import Pricing_light  from './component/Pricing.js';
+
+function App() {
+  return (
+    <div className='App'>
+    <Pricing_light/>
+    </div>
+  );
+}
+
+export default App;
